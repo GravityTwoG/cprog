23139ba7da175dd6ba5aecc00d8082783554bddc
GravityTwoG <crytekov@gmail.com>
2020-11-04 12:13:28 +0700

    fix of sorting pages

--- src/components/Sidebar/Tree.jsx
+++ src/components/Sidebar/Tree.jsx
@@ -2,118 +2,6 @@ import React, { useState } from "react"
 import config from "../../../config"
 import { TreeNode } from "./TreeNode"
 
-const calculateTreeData = edges => {
-  const originalData = config.sidebar.ignoreIndex
-    ? edges.filter(({ node }) => node.fields.slug !== "/")
-    : edges
-
-  const tree = originalData.reduce(
-    (accu, { node }) => {
-      const { slug, title } = node.fields
-      const parts = slug.split("/")
-      let { items: prevItems } = accu
-
-      const slicedParts =
-        config.gatsby && config.gatsby.trailingSlash
-          ? parts.slice(1, -2)
-          : parts.slice(1, -1)
-
-      for (const part of slicedParts) {
-        let tmp = prevItems && prevItems.find(({ label }) => label === part)
-
-        if (tmp) {
-          if (!tmp.items) {
-            tmp.items = []
-          }
-        } else {
-          tmp = { label: part, items: [] }
-          prevItems.push(tmp)
-        }
-        prevItems = tmp.items
-      }
-      const slicedLength =
-        config.gatsby && config.gatsby.trailingSlash
-          ? parts.length - 2
-          : parts.length - 1
-
-      const existingItem = prevItems.find(
-        ({ label }) => label === parts[slicedLength]
-      )
-
-      if (existingItem) {
-        existingItem.url = slug
-        existingItem.title = title
-      } else {
-        prevItems.push({
-          label: parts[slicedLength],
-          url: slug,
-          items: [],
-          title: title || "Undefined",
-        })
-      }
-      return accu
-    },
-    { items: [] }
-  )
-
-  const {
-    sidebar: { forcedNavOrder = [] },
-  } = config
-
-  const tmp = [...forcedNavOrder]
-
-  if (config.gatsby && config.gatsby.trailingSlash) {
-  }
-  tmp.reverse()
-  return tmp.reduce((accu, slug) => {
-    const parts = slug.split("/")
-
-    let { items: prevItems } = accu
-
-    const slicedParts =
-      config.gatsby && config.gatsby.trailingSlash
-        ? parts.slice(1, -2)
-        : parts.slice(1, -1)
-
-    for (const part of slicedParts) {
-      let tmp = prevItems.find(item => item && item.label === part)
-
-      if (tmp) {
-        if (!tmp.items) {
-          tmp.items = []
-        }
-      } else {
-        tmp = { label: part, items: [] }
-        prevItems.push(tmp)
-      }
-      if (tmp && tmp.items) {
-        prevItems = tmp.items
-      }
-    }
-    // sort items alphabetically.
-    prevItems.forEach(item => {
-      item.items = item.items.sort(function (a, b) {
-        if (a.label < b.label) return -1
-        if (a.label > b.label) return 1
-        return 0
-      })
-    })
-    const slicedLength =
-      config.gatsby && config.gatsby.trailingSlash
-        ? parts.length - 2
-        : parts.length - 1
-
-    const index = prevItems.findIndex(
-      ({ label }) => label === parts[slicedLength]
-    )
-
-    if (prevItems.length) {
-      accu.items.unshift(prevItems.splice(index, 1)[0])
-    }
-    return accu
-  }, tree)
-}
-
 export const Tree = ({ edges }) => {
   let [treeData] = useState(() => {
     return calculateTreeData(edges)
@@ -150,4 +38,114 @@ export const Tree = ({ edges }) => {
       {...treeData}
     />
   )
+}
+
+const calculateTreeData = edges => {
+  const originalData = config.sidebar.ignoreIndex
+    ? edges.filter(({ node }) => node.fields.slug !== "/")
+    : edges
+
+  const tree = originalData.reduce(
+    (accu, { node }) => {
+      const { slug, title } = node.fields
+      const parts = slug.split("/")
+
+      const slicedParts =
+        config.gatsby && config.gatsby.trailingSlash
+          ? parts.slice(1, -2)
+          : parts.slice(1, -1)
+
+      let { items: prevItems } = accu
+      for (const part of slicedParts) {
+        let tmp = prevItems && prevItems.find(({ label }) => label === part)
+
+        if (tmp) {
+          if (!tmp.items) {
+            tmp.items = []
+          }
+        } else {
+          tmp = { label: part, items: [] }
+          prevItems.push(tmp)
+        }
+        prevItems = tmp.items
+      }
+
+      const slicedLength =
+        config.gatsby && config.gatsby.trailingSlash
+          ? parts.length - 2
+          : parts.length - 1
+
+      const existingItem = prevItems.find(
+        ({ label }) => label === parts[slicedLength]
+      )
+
+      if (existingItem) {
+        existingItem.url = slug
+        existingItem.title = title
+      } else {
+        prevItems.push({
+          label: parts[slicedLength],
+          url: slug,
+          items: [],
+          title: title || "Undefined",
+        })
+      }
+      return accu
+    },
+    { items: [] }
+  )
+
+  const {
+    sidebar: { forcedNavOrder = [] },
+  } = config
+
+  const tmp = [...forcedNavOrder]
+
+  tmp.reverse()
+  return tmp.reduce((accu, slug) => {
+    const parts = slug.split("/")
+
+    let { items: prevItems } = accu
+
+    const slicedParts =
+      config.gatsby && config.gatsby.trailingSlash
+        ? parts.slice(1, -2)
+        : parts.slice(1, -1)
+
+    for (const part of slicedParts) {
+      let tmp = prevItems.find(item => item && item.label === part)
+
+      if (tmp) {
+        if (!tmp.items) {
+          tmp.items = []
+        }
+      } else {
+        tmp = { label: part, items: [] }
+        prevItems.push(tmp)
+      }
+      if (tmp && tmp.items) {
+        prevItems = tmp.items
+      }
+    }
+    // sort items alphabetically with number compare
+    prevItems.forEach(item => {
+      item.items = item.items.sort((a, b) =>
+        a.label.localeCompare(b.label, "kn", { numeric: true })
+      )
+    })
+
+    const slicedLength =
+      config.gatsby && config.gatsby.trailingSlash
+        ? parts.length - 2
+        : parts.length - 1
+
+    const index = prevItems.findIndex(
+      ({ label }) => label === parts[slicedLength]
+    )
+
+    if (prevItems.length) {
+      accu.items.unshift(prevItems.splice(index, 1)[0])
+    }
+    return accu
+  }, tree)
 }
