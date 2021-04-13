a4c0d8ee5441f4d412e47acfccd694a87a49dd7e
GravityTwog <crytekov@gmail.com>
2021-04-13 22:04:01 +0700

    fix: use service worker only in production

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,12 @@ const plugins = [
 ]
 
 // check and add pwa functionality
-if (config.pwa && config.pwa.enabled && config.pwa.manifest) {
+if (
+  process.env.NODE_END == "production" &&
+  config.pwa &&
+  config.pwa.enabled &&
+  config.pwa.manifest
+) {
   plugins.push({
     resolve: `gatsby-plugin-manifest`,
     options: { ...config.pwa.manifest },
