71c7aef8410d224d84f0645f499ea92d81d52cb7
GravityTwog <crytekov@gmail.com>
2021-06-19 22:27:19 +0700

    refactor

--- src/components/mdxComponents/Pre.jsx
+++ src/components/mdxComponents/Pre.jsx
@@ -1,8 +1,19 @@
 import React from "react"
-import { styled } from '@linaria/react';
+import { styled } from "@linaria/react"
 import { CodeBlock } from "./CodeBlock"
 
-const StyledPre = styled("pre")`
+const CodeSkeleton = styled.div`
+  width: 100%;
+  height: 170px;
+  background: #011627;
+  border-radius: 5px;
+`
+
+const LazyCodeBlock = React.lazy(() =>
+  import("./CodeBlock").then(module => ({ default: module.CodeBlock }))
+)
+
+const StyledPre = styled.pre`
   padding: 16px;
   background: var(--preFormattedTextColor);
   border-radius: 5px;
@@ -13,6 +24,12 @@ const StyledPre = styled("pre")`
 export const Pre = props => {
   if (props.children?.props?.mdxType === "code") {
     return <CodeBlock {...props} />
+    //TODO: Make it lazy
+    // return (
+    //   <React.Suspense fallback={<CodeSkeleton />}>
+    //     <LazyCodeBlock {...props} />
+    //   </React.Suspense>
+    // )
   }
   return <StyledPre {...props} />
 }
