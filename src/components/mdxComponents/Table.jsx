66adbb441a202dfef06aec711e2cb8d120177de9
GravityTwoG <crytekov@gmail.com>
2021-08-13 19:03:36 +0700

    feat: css

--- src/components/mdxComponents/Table.jsx
+++ src/components/mdxComponents/Table.jsx
@@ -7,7 +7,7 @@ const nthBackgroundDark = "#146da9"
 
 const StyledTableWrapper = styled.div`
   display: flex;
-  margin: 0 -60px;
+  margin: 1rem -60px 2.5rem;
   width: calc(100% + 120px);
   overflow-x: auto;
   -webkit-overflow-scrolling: touch;
@@ -15,19 +15,22 @@ const StyledTableWrapper = styled.div`
 
   // pad
   & > div {
+    width: 60px;
     flex: 0 0 60px;
   }
 
   @media only screen and (max-width: 520px) {
-    margin: 0 -16px;
+    margin: 1rem -16px 2.5rem;
     width: calc(100% + 32px);
 
     & > div {
+      width: 16px;
       flex: 0 0 16px;
     }
   }
 
   table {
+    margin: auto;
     border-collapse: collapse;
     border-spacing: 0;
   }
@@ -56,6 +59,7 @@ const StyledTableWrapper = styled.div`
       text-align: left;
       margin: 0;
       padding: 6px 13px;
+      min-width: min-content;
     }
   }
 
@@ -72,6 +76,9 @@ const StyledTableWrapper = styled.div`
     text-align: left;
     margin: 0;
     padding: 6px 13px;
+    min-width: min-content;
+    word-wrap: normal;
+    white-space: pre-wrap;
   }
 `
 
